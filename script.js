0d48849a0a25ee22283af27e693c1e0ecccd69d2
9Foxstails <t-mozer@hotmail.com>
2025-11-24 16:59:14 +0300

    fix: clean up code

--- script.js
+++ script.js
@@ -1,272 +1,222 @@
-/*
-1.Set up your project with HTML, CSS and JavaScript files and get the Git repo all set up.
-2. Have as little as possible global variables and stuff
-3. All logic for winning 3 in a row.
-4. Make game work thorught console first.
-5. Then dom interaction
-6. 
-
-*/
 
 //function that makes a gameboard
-function gameBoard()
-{
-  let rows=3;
-  let columns=3;
-  let board=[];
+function gameBoard() {
+  let rows = 3;
+  let columns = 3;
+  let board = [];
 
-   for (let i = 0; i < rows; i++) 
-  {
+  for (let i = 0; i < rows; i++) {
     board[i] = [];
-    for (let j = 0; j < columns; j++) 
-    {
+    for (let j = 0; j < columns; j++) {
       board[i].push(``);
     }
   }
- 
+
   return board;
 }
 
 //make player function
-function makePlayer(name, id){
-  let score=0;
-  const upScore=()=>score++;
-  const getScore=()=>score;
-  const getName=()=>name;
-  const getId=()=>id;
-  const changeName=(newName)=>{
-    newName=prompt(`Enter a new name :]`);
-    if(newName.length!=0)
-    {
-      name=newName;
+function makePlayer(name, id) {
+  let score = 0;
+  const upScore = () => score++;
+  const getScore = () => score;
+  const getName = () => name;
+  const getId = () => id;
+  const changeName = (newName) => {
+    newName = prompt(`Enter a new name :]`);
+    if (newName.length != 0) {
+      name = newName;
     }
   };
-  return {getName, upScore, getScore, changeName, getId};
+  return { getName, upScore, getScore, changeName, getId };
 }
 
-const playerOne=makePlayer(`Broski`, 1);
-const playerTwo=makePlayer(`Bruv`, 2);
-playerOne.mark="X";
-playerTwo.mark='O';
+const playerOne = makePlayer(`Broski`, 1);
+const playerTwo = makePlayer(`Bruv`, 2);
+playerOne.mark = "X";
+playerTwo.mark = 'O';
 
 //coin toss who goes first
-function coinToss()
-{
-  return Math.floor(Math.random()*2); //produces 0 or 1;
+function coinToss() {
+  return Math.floor(Math.random() * 2); //produces 0 or 1;
 }
 
 
 //game logic
-function gameFlow()
-{
-  let board=gameBoard();
-  let currentPlayer=playerOne;
-  let gameOver=undefined;
-  let winner=undefined;
+function gameFlow() {
+  let board = gameBoard();
+  let currentPlayer = playerOne;
+  let gameOver = undefined;
+  let winner = undefined;
 
-  function switchPlayer()
-  {
-    if (currentPlayer==playerTwo)
-    {
-      currentPlayer=playerOne;
+  function switchPlayer() {
+    if (currentPlayer == playerTwo) {
+      currentPlayer = playerOne;
     }
-    else
-    {
-      currentPlayer=playerTwo;
+    else {
+      currentPlayer = playerTwo;
     }
   }
-  
-  const getCurrentPlayer=()=>currentPlayer.getName();
-  const getCurrentPlayerScore=()=>currentPlayer.getScore();
-  const getCurrentPlayerId=()=>currentPlayer.getId();
-  const changeCurrentPlayerName=(newName)=>currentPlayer.changeName(newName);
-  const getBoard=()=>board;
 
-  function playerChoice(row, column)
-  {
-    //check if inputs are ok
-    if(column<0 || column>2 || typeof column!=`number` || row<0 || row>2 || typeof row!=`number`)
-    {
-      return console.error(`invalid input mate, enter a digit lower than or equal to 3`);
-    }
+  const getCurrentPlayer = () => currentPlayer.getName();
+  const getCurrentPlayerScore = () => currentPlayer.getScore();
+  const getCurrentPlayerId = () => currentPlayer.getId();
+  const changeCurrentPlayerName = (newName) => currentPlayer.changeName(newName);
+  const getBoard = () => board;
 
-    //check if game over
-    if (gameOver || gameOver==undefined)
-    {
-      return console.log(`games over`);
-    }
+  function playerChoice(row, column) 
+  { 
+    let check = boardState();
 
-    let check=boardState();
-
-    if(check==false)
+    if (check == false) 
     {
       return 0;
     }
-    else
+    else 
     {
-      if(board[row][column]==0)
+      if (board[row][column] == 0)
       {
-        board[row][column]=currentPlayer.mark;
+        board[row][column] = currentPlayer.mark;
         boardState();
-        if(gameOver!=true)
+        if (gameOver != true)
         {
-        switchPlayer();
+          switchPlayer();
         }
       }
       else
-      {
-        board[row][column]=board[row][column];
+        {
+        board[row][column] = board[row][column];
         console.log(`cant do that mate!`);
       }
-    } 
+    }
   }
-  
+
   //checks the rows and columns if anyone won or if its a tie cause all fields completed
-  function boardState()
-  {
-    let i=0;
-    let j=0;
+  function boardState() {
+    let i = 0;
+    let j = 0;
 
     // check winning combinations
-    if((board[i][j]==board[i+1][j] && board[i+1][j]==board[i+2][j]) && board[i][j]!=`` ||  //row 1
-       (board[i][j+1]==board[i+1][j+1] && board[i+1][j+1]==board[i+2][j+1]) && board[i][j+1]!=`` ||//row 2
-       (board[i][j+2]==board[i+1][j+2] && board[i+1][j+2]==board[i+2][j+2]) && board[i][j+2]!=`` || //row 3
-       (board[i][j]==board[i][j+1] && board[i][j+1]==board[i][j+2]) && board[i][j]!=`` || //column 1
-       (board[i+1][j]==board[i+1][j+1] && board[i+1][j+1]==board[i+1][j+2]) && board[i+1][j]!=`` || //column 2
-       (board[i+2][j]==board[i+2][j+1] && board[i+2][j+1]==board[i+2][j+2]) && board[i+2][j]!=`` || //column 3
-       (board[i][j]==board[i+1][j+1] && board[i+1][j+1]==board[i+2][j+2]) && board[i][j]!=`` || //diagonal 1
-       (board[i+2][j]==board[i+1][j+1] && board[i+1][j+1]==board[i][j+2]) && board[i+2][j]!=`` //diagonal 2
-      )  
-    {
+    if ((board[i][j] == board[i + 1][j] && board[i + 1][j] == board[i + 2][j]) && board[i][j] != `` ||  //row 1
+      (board[i][j + 1] == board[i + 1][j + 1] && board[i + 1][j + 1] == board[i + 2][j + 1]) && board[i][j + 1] != `` ||//row 2
+      (board[i][j + 2] == board[i + 1][j + 2] && board[i + 1][j + 2] == board[i + 2][j + 2]) && board[i][j + 2] != `` || //row 3
+      (board[i][j] == board[i][j + 1] && board[i][j + 1] == board[i][j + 2]) && board[i][j] != `` || //column 1
+      (board[i + 1][j] == board[i + 1][j + 1] && board[i + 1][j + 1] == board[i + 1][j + 2]) && board[i + 1][j] != `` || //column 2
+      (board[i + 2][j] == board[i + 2][j + 1] && board[i + 2][j + 1] == board[i + 2][j + 2]) && board[i + 2][j] != `` || //column 3
+      (board[i][j] == board[i + 1][j + 1] && board[i + 1][j + 1] == board[i + 2][j + 2]) && board[i][j] != `` || //diagonal 1
+      (board[i + 2][j] == board[i + 1][j + 1] && board[i + 1][j + 1] == board[i][j + 2]) && board[i + 2][j] != `` //diagonal 2
+    ) {
       //need to check what was the winning combination
       currentPlayer.upScore();
-      winner=true;
-      console.log(`${currentPlayer.getName()} won! Score: ${currentPlayer.getScore()}`);
-      gameOver=true;
+      winner = true;
+      gameOver = true;
     }
+    else {
 
-    let tmpArr=board.flat();
-    if (!tmpArr.includes(``))
-    {
-      //console.log(!board.includes(0));
-      console.log(`its a tie! Start a new game`);
-      winner=false;
-      gameOver=true;
-      return false;
+      let tmpArr = board.flat();
+      if (!tmpArr.includes(``)) {
+        winner = false;
+        gameOver = true;
+        return false;
+      }
     }
   }
-  
+
   //reset the board
-  function newGame()
-  {
-    board=gameBoard();
-    gameOver=false;
-    winner=false;
-    if(coinToss()==true)
-    {
-      currentPlayer=playerOne;
+  function newGame() {
+    board = gameBoard();
+    gameOver = false;
+    winner = false;
+    if (coinToss() == true) {
+      currentPlayer = playerOne;
     }
-    else
-    {
-      currentPlayer=playerTwo;
+    else {
+      currentPlayer = playerTwo;
     }
 
     console.log(`${currentPlayer.getName()} starts!`);
   }
 
-  const getGameState=()=>{return {gameOver, winner, getCurrentPlayerScore}};
+  const getGameState = () => { return { gameOver, winner, getCurrentPlayerScore } };
 
-  return {getBoard,getCurrentPlayer, playerChoice, newGame, changeCurrentPlayerName, getGameState, getCurrentPlayerScore, getCurrentPlayerId};
+  return { getBoard, getCurrentPlayer, playerChoice, newGame, changeCurrentPlayerName, getGameState, getCurrentPlayerScore, getCurrentPlayerId };
 };
 
-function displayControl()
-{
-  const game=gameFlow();
-  let boardContainer=document.querySelector(`.board`);
-  let plrOneContainer=document.querySelector(`.player`);
-  let plrTwoContainer=document.querySelector(`.player_2`);
-  let newGameBtn=document.querySelector(`.newGame`);
-  plrOneContainer.firstElementChild.innerHTML=game.getCurrentPlayer();
-  plrTwoContainer.firstElementChild.innerHTML=playerTwo.getName();
+function displayControl() {
+  const game = gameFlow();
+  let boardContainer = document.querySelector(`.board`);
+  let plrOneContainer = document.querySelector(`.player`);
+  let plrTwoContainer = document.querySelector(`.player_2`);
+  let newGameBtn = document.querySelector(`.newGame`);
+  plrOneContainer.firstElementChild.innerHTML = game.getCurrentPlayer();
+  plrTwoContainer.firstElementChild.innerHTML = playerTwo.getName();
 
 
   //updates display
-  function updateScreen()
-  {
-    let board=game.getBoard();
- 
+  function updateScreen() {
+    let board = game.getBoard();
+
     //remove old children
-    while(boardContainer.firstChild)
-    {
+    while (boardContainer.firstChild) {
       boardContainer.removeChild(boardContainer.firstChild);
     }
 
     //add new children
-    for(i=0;i<board.length;i++)
-    {
-      for(j=0;j<board[i].length;j++)
-      {
-      let newDiv=document.createElement(`div`);
-      newDiv.innerHTML=board[i][j];
-      newDiv.dataset.row=i;
-      newDiv.dataset.column=j;
-      boardContainer.appendChild(newDiv);
-      } 
+    for (i = 0; i < board.length; i++) {
+      for (j = 0; j < board[i].length; j++) {
+        let newDiv = document.createElement(`div`);
+        newDiv.innerHTML = board[i][j];
+        newDiv.dataset.row = i;
+        newDiv.dataset.column = j;
+        boardContainer.appendChild(newDiv);
+      }
     }
   }
 
-  function handleTurnDisplay(name){
-    if(name===true)
-    {
+  function handleTurnDisplay(name) {
+    if (name === true) {
       plrOneContainer.lastElementChild.removeAttribute(`id`);
       plrTwoContainer.lastElementChild.removeAttribute(`id`);
     }
 
-    else if(name==playerOne.getName())
-    {
+    else if (name == playerOne.getName()) {
       plrTwoContainer.lastElementChild.removeAttribute(`id`);
-      plrOneContainer.lastElementChild.id=`turn`;
+      plrOneContainer.lastElementChild.id = `turn`;
     }
-    else
-    {
+    else {
       plrOneContainer.lastElementChild.removeAttribute(`id`);
-      plrTwoContainer.lastElementChild.id=`turn`;
+      plrTwoContainer.lastElementChild.id = `turn`;
     }
   }
 
   //handles click on playing board
-  function clickHandlerBoard(e)
-  {
+  function clickHandlerBoard(e) {
 
-    let row=Number(e.target.dataset.row);
-    let column=Number(e.target.dataset.column);
-    console.log(game.getCurrentPlayer());
-    let tmpPlr=game.getCurrentPlayer();
+    let row = Number(e.target.dataset.row);
+    let column = Number(e.target.dataset.column);
+    let tmpPlr = game.getCurrentPlayer();
     game.playerChoice(row, column);
     handleTurnDisplay(tmpPlr);
     updateScreen();
-    
-    let state=game.getGameState();
-    if(state.gameOver)
-    {
-      let dialog=document.querySelector(`dialog`);
-      let closeBtn=document.querySelector(`.closeBtn`);
-      closeBtn.addEventListener(`click`, (e)=>{
+
+    let state = game.getGameState();
+    if (state.gameOver) {
+      let dialog = document.querySelector(`dialog`);
+      let closeBtn = document.querySelector(`.closeBtn`);
+      closeBtn.addEventListener(`click`, (e) => {
         e.preventDefault();
         dialog.close();
       });
       handleTurnDisplay(state.gameOver);
-      if(state.winner)
-      {
-        dialog.firstElementChild.innerHTML=`${game.getCurrentPlayer()} won!`;
+      if (state.winner) {
+        dialog.firstElementChild.innerHTML = `${game.getCurrentPlayer()} won!`;
         dialog.showModal();
-        let id=game.getCurrentPlayerId();
-        let scoreDiv=document.querySelector(`[data-id="${id}"]`).nextElementSibling;
-        scoreDiv.innerHTML=`${game.getCurrentPlayerScore()}`;
+        let id = game.getCurrentPlayerId();
+        let scoreDiv = document.querySelector(`[data-id="${id}"]`).nextElementSibling;
+        scoreDiv.innerHTML = `${game.getCurrentPlayerScore()}`;
       }
-      else
-      {
-        dialog.firstElementChild.innerHTML=`It's a tie!`;
+      else {
+        dialog.firstElementChild.innerHTML = `It's a tie!`;
         dialog.showModal();
       }
 
@@ -275,8 +225,7 @@ function displayControl()
   }
 
   //handles click to start new game
-  function clickHandlerNewGame()
-  {
+  function clickHandlerNewGame() {
     game.newGame();
     boardContainer.addEventListener(`click`, clickHandlerBoard);
     handleTurnDisplay(game.getCurrentPlayer());
@@ -284,17 +233,14 @@ function displayControl()
   }
 
   //handles click to change name
-  function clickHandlerChangeName(e)
-  {
-    if(e.target.dataset.id==`1`)
-    {
+  function clickHandlerChangeName(e) {
+    if (e.target.dataset.id == `1`) {
       playerOne.changeName();
-      plrOneContainer.firstElementChild.innerHTML=playerOne.getName();
+      plrOneContainer.firstElementChild.innerHTML = playerOne.getName();
     }
-    else if(e.target.dataset.id==`2`)
-    {
+    else if (e.target.dataset.id == `2`) {
       playerTwo.changeName();
-      plrTwoContainer.firstElementChild.innerHTML=playerTwo.getName();
+      plrTwoContainer.firstElementChild.innerHTML = playerTwo.getName();
     }
   }
 
@@ -302,7 +248,6 @@ function displayControl()
   newGameBtn.addEventListener(`click`, clickHandlerNewGame);
   plrOneContainer.firstElementChild.addEventListener(`click`, clickHandlerChangeName);
   plrTwoContainer.firstElementChild.addEventListener(`click`, clickHandlerChangeName);
-
 
   updateScreen();
 }
